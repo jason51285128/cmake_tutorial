48acd104afc1d0390d8f1c7176b30de0174f7e8d
zhangchen <zhangchenwhu@qq.com>
2020-03-21 20:58:57 +0800

    support system introspection

--- MathFunctions/mysqrt.cxx
+++ MathFunctions/mysqrt.cxx
@@ -1,3 +1,11 @@
+#include <cmath>
+#include "TutorialConfig.h"
+
 double mysqrt(double x) {
-  return x;
+#if defined(HAVE_LOG) && defined(HAVE_EXP)
+  double result = x;
+#else
+  double result = x;
+#endif
+  return result;
 }
